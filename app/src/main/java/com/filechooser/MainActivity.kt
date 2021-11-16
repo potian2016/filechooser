a12273e167f14f54c6487650ac6ed5e8f5fba94a
破天荒 <646367625@qq.com>
2021-11-16 16:12:57 +0800

    update:first commit

---
+++ app/src/main/java/com/filechooser/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.filechooser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thl.filechooser.FileChooser
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
